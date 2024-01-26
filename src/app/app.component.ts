137a2f304c0a7b53e5d61430e71a451cdac161a2
Priyobrato.Das <priyodas12@gmail.com>
2024-01-26 15:31:49 +0530

    feat:basic validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule, NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,8 @@ import { FormsModule, NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'angular-form-v1';
 
+  @ViewChild('formData', { static: true }) signupForm!: NgForm;
+
   userdata: any = {
     username: '',
     email: '',
@@ -22,6 +25,7 @@ export class AppComponent {
   constructor() {
     console.log("form component loaded!");
   }
+
   resetForm() {
     this.userdata = {
       username: '',
@@ -29,9 +33,14 @@ export class AppComponent {
       gender: '',
     }
   }
-  onFormSubmit(f: NgForm) {
+
+  validateEmail(email: string) {
+    console.log("validating email value...");
+  }
+
+  onFormSubmit() {
     console.log("Form Submitted!");
-    console.log(f);
+    console.log(this.signupForm.controls['email'].valid);
     console.log(this.userdata);
     this.resetForm();
   }
